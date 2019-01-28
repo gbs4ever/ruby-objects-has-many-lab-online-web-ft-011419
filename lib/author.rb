d1568159329a0519093c138af3e97a8741b0ee77
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-28 19:04:36 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,22 +1,19 @@
 class Author
 
 attr_accessor :name , :posts
-  def initialize(name)
+    def initialize(name)
     @name = name
-  @posts = []
-  end
+      @posts = []
+    end
 
-  def add_post(post)
-@post << post
-post.author= self
-  end
+    def add_post(post)
+      @post << post
+      post.author= self
+    end
 
 
 
-  def add_song(song)
-     @songs <<song
-  song.artist = self
-   end
+
 
 
 
