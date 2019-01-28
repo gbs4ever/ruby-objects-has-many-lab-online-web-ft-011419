8439df6b255160698cf540d2c12ad19426991a96
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-28 18:58:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,14 @@
 class Author
 
-attr_accessor :name ,:posts
+attr_accessor :name 
+attr_ reader :posts
   def initialize(name)
     @name = name
   @posts = []
+  end
+
+  def add_post(name)
+    
   end
 
 
